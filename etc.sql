2c85a43ca647ff2e135e8c5688a2ebdc0de6ce5f
argl <andi@watwet.com>
2010-04-30 16:51:11 +0200

    added database definitions

---
+++ etc.sql
@@ -0,0 +1,104 @@
+﻿
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table drivers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `drivers`;
+
+CREATE TABLE `drivers` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table events
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL auto_increment,
+  `track` varchar(100) collate utf8_unicode_ci default NULL,
+  `mod` varchar(100) collate utf8_unicode_ci default NULL,
+  `season_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table races
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `races`;
+
+CREATE TABLE `races` (
+  `id` int(11) NOT NULL auto_increment,
+  `race_time` varchar(50) collate utf8_unicode_ci default NULL,
+  `event_id` int(11) default NULL,
+  `laps` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `event_id` (`event_id`),
+  KEY `race_time` (`race_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table results
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `results`;
+
+CREATE TABLE `results` (
+  `id` int(11) NOT NULL auto_increment,
+  `race_id` int(11) default NULL,
+  `driver_id` int(11) default NULL,
+  `race_time` double default NULL,
+  `finish_status` varchar(100) collate utf8_unicode_ci default NULL,
+  `best_lap` double default NULL,
+  `laps_completed` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `race_id` (`race_id`),
+  KEY `driver_id` (`driver_id`),
+  KEY `race_time` (`race_time`),
+  KEY `finish_status` (`finish_status`),
+  KEY `best_lap` (`best_lap`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table seasons
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `seasons`;
+
+CREATE TABLE `seasons` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(150) collate utf8_unicode_ci default NULL,
+  `hide` tinyint(4) default '0',
+  PRIMARY KEY  (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
